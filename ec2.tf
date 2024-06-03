2f0ac31001dabecc34e0f25940bd3768e4b4b170
sonnguyenHT <sondzvl129@gmail.com>
2024-06-03 10:55:54 +0700

    change ec2 to private subnet, attach allow icmp security group, change vpc endpoint to private subnet

--- ec2.tf
+++ ec2.tf
@@ -9,10 +9,12 @@ module "ec2_instance" {
   #instance_type          = "c5.large" # this is using when turn on placement group
   key_name               = "sonnguyen"
   monitoring             = true
-  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  subnet_id              = module.vpc.public_subnets[0] # Use public subnet from the VPC module
-  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}","${aws_security_group.allow_icmp.id}"]
+  #subnet_id              = module.vpc.public_subnets[0] # Use public subnet from the VPC module
+  subnet_id = module.vpc.private_subnets[0] # Use private subnet from the VPC module
+  associate_public_ip_address = false # Use private IP
   #placement_group = aws_placement_group.cluster.name
+  iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
   tags = {
     Terraform   = "true"
     Environment = "dev"
