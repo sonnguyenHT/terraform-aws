5d0794a31ccf58b545def7470bbc641567dcff3e
sonnguyenHT <sondzvl129@gmail.com>
2024-06-03 10:52:04 +0700

    add allow icmp rule

--- security_groups.tf
+++ security_groups.tf
@@ -21,3 +21,20 @@ resource "aws_vpc_security_group_egress_rule" "allow_all" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+resource "aws_security_group" "allow_icmp" {
+  name = "allow_icmp"
+  description = "Allow ICMP inbound traffic"
+  vpc_id = module.vpc.vpc_id
+  tags = {
+    Name = "allow_icmp"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp" {
+  security_group_id = aws_security_group.allow_icmp.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "icmp"
+  from_port = -1
+  to_port = -1
+}
