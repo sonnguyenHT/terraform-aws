7e845bf59738c25b85456b1f06b4ae629640cb09
sonnguyenHT <sondzvl129@gmail.com>
2024-06-03 10:54:54 +0700

    create ec2 instance profile

---
+++ iam.tf
@@ -0,0 +1,30 @@
+resource "aws_iam_instance_profile" "ec2_ssm_profile" {
+  name = "ec2SessionManager"
+  role = aws_iam_role.role.name
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "aws_ec2_session_manager_role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_ssm_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.role.name
+}
+
+#AmazonSSMManagedInstanceCore
